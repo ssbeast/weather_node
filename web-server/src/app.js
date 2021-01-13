394bd2c58514dd5ce414e5e966af78b6d5bf046f
ssbeast <ssbeast007@gmail.com>
2021-01-13 20:39:46 +0530

    404 page  added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -43,6 +43,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Saurabh Sahu',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Saurabh Sahu',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
